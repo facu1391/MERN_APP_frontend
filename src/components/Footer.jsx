c81232c6897581d7692953d7d0dc4b75292d13fb
Facundo <facurael@gmail.com>
2023-06-12 20:22:34 -0300

    Deployment

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+
+
+
+
+const Footer = () => {
+    return (
+      <footer className="py-10">
+        <p className="text-center font-bold">APP - Adminitrador de {''} <span className="text-indigo-600">Pacientes</span> </p>
+      </footer>
+    )
+  }
+  
+  export default Footer;
