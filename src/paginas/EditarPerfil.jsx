048cc237618d15b7699ba2c2ba339b8a0dbb88d5
Facundo <facurael@gmail.com>
2023-10-26 15:53:10 -0300

    fix(EditarPerfil):cambiod en setAlerta

--- src/paginas/EditarPerfil.jsx
+++ src/paginas/EditarPerfil.jsx
@@ -107,7 +107,7 @@ const EditarPerfil = () => {
 
                     <input type="submit"
                            value="Guardar Cambios"  
-                           className="bg-indigo-700 px-10 py-3 font-bold text-white rounded-lg uppercase w-full mt-5"
+                           className="bg-indigo-700 px-10 py-3 font-bold text-white rounded-lg uppercase w-full mt-5 hover:cursor-pointer hover:bg-indigo-800"
                     />
                 </form>
             </div>
